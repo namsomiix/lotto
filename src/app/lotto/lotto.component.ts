aef18b47828e387d518f0843e9ed9f9b564d7296
Chalita Boonmak <namsomiix@gmail.com>
2019-02-23 15:28:07 +0700

    add service

--- src/app/lotto/lotto.component.ts
+++ src/app/lotto/lotto.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { LottoService } from '../lotto.service';
 import { Lotto } from '../lotto';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-lotto',
@@ -12,8 +13,9 @@ import { Lotto } from '../lotto';
 export class LottoComponent implements OnInit {
   title = 'Lotto';
   formGroup: FormGroup;
-
   result : Lotto[] = [];
+  wasBigPrize: boolean = false;
+  wasLastThreeDigitPrize:boolean = false;
 
   constructor(private formBuilder:FormBuilder,private lottoService:LottoService) { }
 
@@ -26,12 +28,44 @@ export class LottoComponent implements OnInit {
   }
 
   checkLotto(){
+
+    this.wasBigPrize = false;
+    this.wasLastThreeDigitPrize = false;
+
     if(this.formGroup.invalid){
       alert("Error !!!");
     }
     else
     {
       this.result = this.lottoService.getAll();
+
+      const period = this.formGroup.get('period').value;
+      const lottoNumber: string = this.formGroup.get('lottoNumber').value;
+
+      const lotto = this.result.find((item:Lotto) => {
+        return item.period === period;
+      });
+
+      if(lotto){
+        if(lotto.bigPrize === lottoNumber){
+          this.wasBigPrize = true;
+        }
+        if(lotto.lastThreeDigitPrize ===lottoNumber.substr(3))
+        {
+          this.wasLastThreeDigitPrize = true;
+        }
+      }
+
     }
+  }
+  onClear(){
+    this.wasBigPrize = false;
+    this.wasLastThreeDigitPrize = false;
+    this.formGroup.reset();
+    //this.formGroup.get('period').setValue('');
+    //this.formGroup.get('lottoNumber').setValue('');
+    //this.formGroup.controls.period.setValue('');
+    //this.formGroup.controls.lottoNumber.setValue('');
+    this.result = [];
   }
 }
